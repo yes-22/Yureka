24c503a6a62c98bf5c6d1e8db0ccb803613ee786
eunseo.yoo <yooes0220@sookmyung.ac.kr>
2024-12-30 01:17:58 +0900

    success

--- 2753.py
+++ 2753.py
@@ -1,9 +1,8 @@
 year = int(input())
 
-if year % 4 == 0:
-    if year % 100 != 0 & year % 400 == 0:
-        print("1")
-    else:
-        print("0")
+#4의 배수이면서 100의 배수는 아니여야 함
+#400의 배수는 ㄱㅊ
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(1) #" " 제외하고 작성해야함
 else:
-    print("0")
+    print(0) 
