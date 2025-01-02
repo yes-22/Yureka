659511b57cb76d941cd9e77eaba54c6d4c6da30f
eunseo.yoo <yooes0220@sookmyung.ac.kr>
2025-01-02 13:53:39 +0900

    success

--- Bronze/21918.py
+++ Bronze/21918.py
@@ -9,14 +9,21 @@ def cmd(a, b, c) :
     elif a == 2 :
         for i in range(b-1, c):
             if bulb_list[i] == 0 :
-                bulb_list[i] == 1
+                bulb_list[i] = 1
             else :
-                bulb_list[i] == 0
+                bulb_list[i] = 0
     elif a == 3 :
         for i in range(b-1, c):
-            bulb_list[i] == 0
+            bulb_list[i] = 0
     elif a == 4 :
         for i in range(b-1, c):
-            bulb_list[i] == 1
+            bulb_list[i] = 1
 
-print(*bulb_list)
+#a,b,c를 묶는 cmd 정의를 안해줬단걸 이제 알았어유...
+for _ in range(M):
+    a, b, c = map(int, input().split()) 
+    cmd(a, b, c)
+
+#리스트 출력 
+for i in bulb_list:
+    print(i, end=' ')
