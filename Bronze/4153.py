62dffa93089c0f2bdeed12f3824c7925dc7367b5
eunseo.yoo <yooes0220@sookmyung.ac.kr>
2025-01-03 12:18:13 +0900

    success

--- Bronze/4153.py
+++ Bronze/4153.py
@@ -1,2 +1,9 @@
 #4153.py
 
+a, b, c = input().split()
+
+while a != 0:
+    if a**2 == b**2 + c**2 or b**2 == a**2 + c**2 or c**2 == a**2 + b**2:
+        print("right")
+    else:
+        print("wrong")
