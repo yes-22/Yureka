1b1dca0d7057e6f13f12f476183d5479aaebc849
eunseo.yoo <yooes0220@sookmyung.ac.kr>
2025-01-03 14:44:13 +0900

    again

--- Bronze/10989.py
+++ Bronze/10989.py
@@ -1,12 +1,13 @@
 #10989.py
+import sys
+
 
 n = int(input())
-num_list = []
+num_list = [0 for i in range(10001)]
 
 for i in range(n):
-    num_list.append(int(input()))
-
-num_list.sort()
-
+    num_list[n] += 1
+    
 for i in range(len(num_list)):
-    print(num_list[i])
+    for j in range(num_list[i]):
+        print(i)
